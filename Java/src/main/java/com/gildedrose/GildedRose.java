99f48956a286cb4cc8651b351a8d05285a310705
eichhorn <eichhorn@encoway.de>
2020-09-30 21:53:47 +0200

    - added skip condition for sulfuras items to remove complexity in remaining algorithm

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -14,11 +14,12 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
+            if (ItemType.SULFURAS.equals(item.itemType)) {
+                continue;
+            }
             if (!ItemType.AGED_BRIE.equals(item.itemType)
                     && !ItemType.BACKSTAGE_PASS.equals(item.itemType)) {
-                if (!ItemType.SULFURAS.equals(item.itemType)) {
-                    item.quality = Math.max(MIN_QUALITY, item.quality - 1);
-                }
+                item.quality = Math.max(MIN_QUALITY, item.quality - 1);
             } else {
                 if (item.quality < MAX_QUALITY) {
                     item.quality = item.quality + 1;
@@ -35,16 +36,12 @@ class GildedRose {
                 }
             }
 
-            if (!ItemType.SULFURAS.equals(item.itemType)) {
-                item.sellIn = item.sellIn - 1;
-            }
+            item.sellIn = item.sellIn - 1;
 
             if (item.sellIn < 0) {
                 if (!ItemType.AGED_BRIE.equals(item.itemType)) {
                     if (!ItemType.BACKSTAGE_PASS.equals(item.itemType)) {
-                        if (!ItemType.SULFURAS.equals(item.itemType)) {
-                            item.quality = Math.max(MIN_QUALITY, item.quality - 1);
-                        }
+                        item.quality = Math.max(MIN_QUALITY, item.quality - 1);
                     } else {
                         item.quality = MIN_QUALITY;
                     }
