839d57a8f05abbb0e6c956e0a441a890055838c4
eichhorn <eichhorn@encoway.de>
2020-09-30 22:27:30 +0200

    - implement strategy for backstage pass and remove backstage pass logic from GildedRose - change if condition for executing strategy to optional.ifPresent

---
+++ Java/src/main/java/com/gildedrose/BackstagePassItemDaySimulationStrategy.java
@@ -0,0 +1,32 @@
+package com.gildedrose;
+
+public class BackstagePassItemDaySimulationStrategy implements ItemDaySimulationStrategy {
+
+    public static final int TEN_DAYS_THRESHOLD = 10;
+    public static final int FIVE_DAYS_THRESHOLD = 5;
+
+    @Override
+    public void simulate(Item item) {
+        if (item.quality < GildedRose.MAX_QUALITY) {
+            item.quality = item.quality + 1;
+
+            if (ItemType.BACKSTAGE_PASS.equals(item.itemType)) {
+                if (item.sellIn <= TEN_DAYS_THRESHOLD) {
+                    item.quality = Math.min(GildedRose.MAX_QUALITY, item.quality + 1);
+                }
+
+                if (item.sellIn <= FIVE_DAYS_THRESHOLD) {
+                    item.quality = Math.min(GildedRose.MAX_QUALITY, item.quality + 1);
+                }
+            }
+        }
+
+        item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0) {
+            if (ItemType.BACKSTAGE_PASS.equals(item.itemType)) {
+                item.quality = GildedRose.MIN_QUALITY;
+            }
+        }
+    }
+}
--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -2,19 +2,20 @@ package com.gildedrose;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 class GildedRose {
 
     public static final int MAX_QUALITY = 50;
     public static final int MIN_QUALITY = 0;
-    public static final int TEN_DAYS_THRESHOLD = 10;
-    public static final int FIVE_DAYS_THRESHOLD = 5;
+
     Item[] items;
 
     private static final Map<ItemType, ItemDaySimulationStrategy> simulationStrategies = new HashMap<ItemType, ItemDaySimulationStrategy>() {{
         put(ItemType.SULFURAS, new SulfurasItemDaySimulationStrategy());
         put(ItemType.STANDARD, new StandardItemDaySimulationStrategy());
         put(ItemType.AGED_BRIE, new AgedBrieItemDaySimulationStrategy());
+        put(ItemType.BACKSTAGE_PASS, new BackstagePassItemDaySimulationStrategy());
     }};
 
     public GildedRose(Item[] items) {
@@ -23,31 +24,8 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (simulationStrategies.containsKey(item.itemType)) {
-                simulationStrategies.get(item.itemType).simulate(item);
-                continue;
-            }
-            if (item.quality < MAX_QUALITY) {
-                item.quality = item.quality + 1;
-
-                if (ItemType.BACKSTAGE_PASS.equals(item.itemType)) {
-                    if (item.sellIn <= TEN_DAYS_THRESHOLD) {
-                        item.quality = Math.min(MAX_QUALITY, item.quality + 1);
-                    }
-
-                    if (item.sellIn <= FIVE_DAYS_THRESHOLD) {
-                        item.quality = Math.min(MAX_QUALITY, item.quality + 1);
-                    }
-                }
-            }
-
-            item.sellIn = item.sellIn - 1;
-
-            if (item.sellIn < 0) {
-                if (ItemType.BACKSTAGE_PASS.equals(item.itemType)) {
-                    item.quality = MIN_QUALITY;
-                }
-            }
+            Optional.of(simulationStrategies.get(item.itemType))
+                    .ifPresent(itemDaySimulationStrategy -> itemDaySimulationStrategy.simulate(item));
         }
     }
 }
